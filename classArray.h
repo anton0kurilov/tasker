1d2d92ce2b6be1fae596ba65b31bc3b089aa8e31
Anton Kurilov <anton0kurilov@gmail.com>
2017-06-26 09:28:11 +0700

    Release

--- classArray.h
+++ classArray.h
@@ -5,8 +5,10 @@ template <class T>
 class Array {
 public:
 	Array() {}
-	~Array() {}
-	Array(char *doc);
+	~Array() {
+		delete[] pType;
+	}
+	Array(char * address);
 	Array(int itsSize);
 	T & operator[](int offSet) { 
 		return pType[offSet]; 
@@ -18,12 +20,12 @@ public:
 		return itsSize; 
 	}
 	void taskIn();
-	void taskSave(char *doc);
+	void taskSave(char * address);
 	bool taskOutOnScreen();
 	bool taskCategoryOut(int categoryMenuResult);
 	bool taskDelete(int taskDeleteNum);
 	bool taskEdit(int taskEditNum);
-	void taskAdd(int taskAddNum);
+	bool taskAdd(int taskAddNum);
 private:
 	T * pType;
 	int itsSize;
@@ -35,12 +37,47 @@ void Array<T>::taskIn() {
 		pType[i].In();
 	}
 }
+// Метод вывода задач определенной категории
+template <class T>
+bool Array<T>::taskCategoryOut(int categoryMenuResult) {
+	char * taskCategoryOut;
+	int i = 0, j = 0;
+	if (categoryMenuResult == 1) {
+		taskCategoryOut = "Личные";
+	}
+	else if (categoryMenuResult == 2) {
+		taskCategoryOut = "Рабочие";
+	}
+	else if (categoryMenuResult == 3) {
+		taskCategoryOut = "Покупки";
+	}
+	else if (categoryMenuResult == 4) {
+		taskCategoryOut = "Другие";
+	}
+	else {
+		taskCategoryOut = "";
+	}
+	for (i; i < itsSize; i++) {
+		if (strcmp(pType[i].taskCategory, taskCategoryOut) == 0) {
+			j += 1;
+			cout << j << ". ";
+			pType[i].Out();
+		}
+	}
+	if (j > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 // Метод вывода задач на дисплей
 template <class T>
 bool Array<T>::taskOutOnScreen() {
 	system("cls");
 	for (int i = 0, j = 1; i < itsSize; i++, j++) {
-		cout  << j << ". ";
+		cout << j << ". ";
 		pType[i].Out();
 	}
 	if (itsSize < 1) {
@@ -52,8 +89,8 @@ bool Array<T>::taskOutOnScreen() {
 }
 // Метод сохранения задачи
 template <class T>
-void Array<T>::taskSave(char *doc) {
-	ofstream taskSaveObj(doc);
+void Array<T>::taskSave(char * address) {
+	ofstream taskSaveObj(address);
 	taskSaveObj << itsSize << endl;
 	for (int i = 0; i < itsSize; i++) {
 		taskSaveObj << pType[i] << endl;
@@ -73,68 +110,6 @@ bool Array<T>::taskDelete(int taskDeleteNum) {
 		return false;
 	}
 }
-// Метод вывода задач определенной категории
-template <class T>
-bool Array<T>::taskCategoryOut(int categoryMenuResult) {
-	system("cls");
-	switch (categoryMenuResult) {
-	case 1: {
-		char tas[] = "Личные";
-		for (int i = 0; i < itsSize; i++) {
-			if (strcmp(pType[i].taskCategory, tas) == 0) {
-				pType[i].Out();
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		break;
-	}
-	case 2: {
-		char tas[] = "Рабочие";
-		for (int i = 0; i < itsSize; i++) {
-			if (strcmp(pType[i].taskCategory, tas) == 0) {
-				pType[i].Out();
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		break;
-	}
-	case 3: {
-		char tas[] = "Покупки";
-		for (int i = 0; i < itsSize; i++) {
-			if (strcmp(pType[i].taskCategory, tas) == 0) {
-				pType[i].Out();
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		break;
-	}
-	case 4: {
-		char tas[] = "Другие";
-		for (int i = 0; i < itsSize; i++) {
-			if (strcmp(pType[i].taskCategory, tas) == 0) {
-				pType[i].Out();
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		break;
-	} 
-	default: {
-		return false;
-	}
-	}
-}
 // Метод редактирования задачи
 template <class T>
 bool Array<T>::taskEdit(int taskEditNum) {
@@ -149,19 +124,23 @@ bool Array<T>::taskEdit(int taskEditNum) {
 }
 // Метод добавления задачи
 template <class T>
-void Array<T>::taskAdd(int taskAddNum) {
-	T * p;
-	p = new T[itsSize + taskAddNum];
-	for (int i = 0; i < itsSize; i++) {
-		p[i] = pType[i];
+bool Array<T>::taskAdd(int taskAddNum) {
+	if (taskAddNum > 20) {
+		return false;
 	}
-	for (int i = itsSize; i < itsSize + taskAddNum; i++) {
-		T taskAddObj;
-		taskAddObj.In();
-		p[i] = taskAddObj;
+	else {
+		T * p;
+		p = new T[itsSize + taskAddNum];
+		for (int i = 0; i < itsSize; i++) {
+			p[i] = pType[i];
+		}
+		for (int i = itsSize; i < itsSize + taskAddNum; i++) {
+			p[i].In();
+		}
+		itsSize = itsSize + taskAddNum;
+		pType = p;
+		return true;
 	}
-	itsSize = itsSize + taskAddNum;
-	pType = p;
 }
 template <class T>
 Array<T>::Array(int size)
@@ -169,9 +148,9 @@ Array<T>::Array(int size)
 	pType = new T[size];
 }
 template <class T>
-Array<T>::Array(char * doc) {
+Array<T>::Array(char * address) {
 	welcome();
-	ifstream fileIn(doc);
+	ifstream fileIn(address);
 	if (fileIn.is_open()) {
 		fileIn >> itsSize;
 		pType = new T[itsSize];
@@ -184,10 +163,7 @@ Array<T>::Array(char * doc) {
 		cin >> itsSize;
 		pType = new T[itsSize];
 		for (int i = 0; i < itsSize; i++) {
-			T ArrayObj;
-			ArrayObj.In();
-			pType[i] = ArrayObj;
-
+			pType[i].In();
 		}
 	}
 }
